c92f4a06d4ad40cbe10a9d0310e8c0a4fed5149b
muziran <gys54818901@gmail.com>
2025-12-24 22:52:46 +0800

    初始化后端

---
+++ src/main/java/com/xjzai1/xjzai1picturebackend/model/vo/PictureVo.java
@@ -0,0 +1,123 @@
+package com.xjzai1.xjzai1picturebackend.model.vo;
+
+import cn.hutool.json.JSONUtil;
+import com.xjzai1.xjzai1picturebackend.model.domain.Picture;
+import lombok.Data;
+import org.springframework.beans.BeanUtils;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+public class PictureVo implements Serializable {
+    /**
+     * id
+     */
+    private String id;
+
+    /**
+     * 图片 url
+     */
+    private String url;
+
+    /**
+     * 缩略图 Url
+     */
+    private String thumbnailUrl;
+
+    /**
+     * 原图 Url
+     */
+    private String originalUrl;
+
+    /**
+     * 图片名称
+     */
+    private String name;
+
+    /**
+     * 简介
+     */
+    private String introduction;
+
+    /**
+     * 分类
+     */
+    private String category;
+
+    /**
+     * 图片体积
+     */
+    private Long pictureSize;
+
+    /**
+     * 图片宽度
+     */
+    private Integer pictureWidth;
+
+    /**
+     * 图片高度
+     */
+    private Integer pictureHeight;
+
+    /**
+     * 图片格式
+     */
+    private String pictureFormat;
+
+    /**
+     * 用户 id
+     */
+    private String userId;
+
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 编辑时间
+     */
+    private Date editTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    /**
+     * 创建用户信息
+     */
+    private UserVo user;
+
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 封装类转对象
+     */
+    public static Picture voToObj(PictureVo pictureVo) {
+        if (pictureVo == null) {
+            return null;
+        }
+        Picture picture = new Picture();
+        BeanUtils.copyProperties(pictureVo, picture);
+        return picture;
+    }
+
+    /**
+     * 对象转封装类
+     */
+    public static PictureVo objToVo(Picture picture) {
+        if (picture == null) {
+            return null;
+        }
+        PictureVo pictureVo = new PictureVo();
+        BeanUtils.copyProperties(picture, pictureVo);
+        return pictureVo;
+    }
+
+}
